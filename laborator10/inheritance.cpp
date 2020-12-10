00650bb42a14dbd8a7b567df500b73ba8f34b86c
Sebastian Haias <sebii.haias@yahoo.com>
2020-12-10 21:13:52 +0200

    Class Parrot

--- laborator10/inheritance.cpp
+++ laborator10/inheritance.cpp
@@ -1,14 +1,49 @@
 #include <iostream>
- 
+#include <vector>
+#include <string>
+
 using namespace std;
 
 class Bird {
     public:
-        virtual void fly();
-        virtual void sound();
-}
+        virtual void fly() = 0;
+        virtual void sound() = 0;
+};
+
+class Parrot: public Bird {
+    public:
+        std::vector<std::string> words;
+
+        Parrot(std::vector<std::string> words)
+        : words(words)
+        { };
+
+        void virtual fly() {
+            cout << "I can fly 10 kilometers\n";
+        };
+
+        void virtual sound() {
+            cout << "prrrrrrr\n";
+        };
+
+        void printWords() {
+            cout << "These are the words i know: \n";
+            for (int i = 0; i < words.size(); i++) {
+                cout << words[i] << "\n";
+            };
+        };
+};
 
 int main() {
 
+    std::vector<std::string> words;
+    words.push_back("Mama");
+    words.push_back("Tata");
+    words.push_back("Papa");
+
+    Parrot *parrot = new Parrot(words);
+    parrot->fly();
+    parrot->sound();
+    parrot->printWords();
     return 0;
 }
